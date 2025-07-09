afc6a1087c483ef49142ec65df42570dcef5d321
Cody Carlson <codycarlson@microsoft.com>
2025-07-09 08:59:34 -0600

    cat facts

--- .github/actions/cat-facts/src/main.py
+++ .github/actions/cat-facts/src/main.py
@@ -1,9 +1,11 @@
 import requests
 import random
 import sys
+import os
 
 # Make an HTTP GET request to the cat-fact API
-cat_url = "https://www.catfacts.net/api/"
+# Using a more reliable API endpoint that returns an array of cat facts
+cat_url = "https://catfact.ninja/facts?limit=10"
 r = requests.get(cat_url)
 r_obj_list = r.json()
 
@@ -11,14 +13,14 @@ r_obj_list = r.json()
 # This will make it easy to select a random one later
 fact_list = []
 
-# Add the "fact" of every object into the fact_list list
-for fact in r_obj_list["facts"]:
+# Add each fact to our fact_list
+for fact in r_obj_list["data"]:
     fact_list.append(fact["fact"])
 
 # Select a random fact from the fact_list and return it
 # into a variable named random_fact so we can use it
 def select_random_fact(fact_arr):
-    return fact_arr[random.randint(0, len(fact_list)-1)]
+    return fact_arr[random.randint(0, len(fact_arr)-1)]
 
 random_fact = select_random_fact(fact_list)
 
@@ -26,4 +28,5 @@ random_fact = select_random_fact(fact_list)
 print(random_fact)
 
 # Set the fact-output of the action as the value of random_fact
-print(f"fact={random_fact}")
+# This is the correct format for GitHub Actions outputs from a Docker container
+print(f"::set-output name=fact::{random_fact}")
